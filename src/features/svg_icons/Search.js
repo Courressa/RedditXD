e74c152d12442d974493842810e74b93aedab2b1
Courressa <courressamalcolm2030@gmail.com>
2024-11-27 18:56:03 -0500

    add styling google font

---
+++ src/features/svg_icons/Search.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./IconsStyle.module.css";
+
+function Search () {return (
+        <div>
+            <svg
+                className={styles.comment}
+                width="100mm"
+                height="100mm"
+                version="1.1"
+                viewBox="0 0 99.999999 100"
+                id="svg1"
+                xmlns="http://www.w3.org/2000/svg">
+                <defs
+                    id="defs1" />
+                <g
+                    id="layer3">
+                    <circle
+                    fill="currentColor"
+                    stroke="#000000"
+                    strokeWidth="1"
+                    strokeLinecap="square"
+                    strokeDasharray="none"
+                    id="path1"
+                    cx="44.029961"
+                    cy="42.976254"
+                    r="38.561169" />
+                    <path
+                    fill="currentColor"
+                    stroke="#000000"
+                    strokeWidth="1"
+                    strokeLinecap="square"
+                    d="M 71.351118,70.29741 91.898434,90.844726"
+                    id="path2" />
+                </g>
+                </svg>
+        </div>
+    )
+}
+
+export {Search};
